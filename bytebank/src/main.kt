6dff1ff8c50e0e162701df2bb494ed36c3a9cbbc
helciodasilva <helciodasilva@gmail.com>
2020-10-13 11:05:14 -0300

    Aula 1: Primeiros Passos

---
+++ bytebank/src/main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val titular: String = "Alex"
+    val numeroConta: Int = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
+}
