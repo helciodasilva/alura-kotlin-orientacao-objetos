e5f5d456f42a1dad8dacba71ac66a180430caef3
helciodasilva <helciodasilva@gmail.com>
2020-10-15 05:19:20 -0300

    Aula 2: Controle de fluxo e o paradigma procedural

--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -1,11 +1,47 @@
-fun main(){
-    val titular: String = "Alex"
-    val numeroConta: Int = 1000
-    var saldo = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
+fun main() {
 
-    println("titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo da conta $saldo")
+    println("Bem vindo ao Bytebank")
+
+    var i = 0
+    while (i < 5) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
+    }
+
+//    for (i in 5 downTo 1) {
+//        val titular: String = "Alex $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = i + 10.0
+//
+//        println("titular $titular")
+//        println("número da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+//    }
+
+//    testaCondicoes(saldo)
+}
+
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("cpmta é positiva")
+    } else if (saldo == 0.0) {
+        println("cpmta é neutra")
+    } else {
+        println("cpmta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("cpmta é positiva")
+        saldo == 0.0 -> println("cpmta é neutra")
+        else -> println("cpmta é negativa")
+    }
 }
