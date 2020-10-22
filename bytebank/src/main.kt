4e327a496f7ea7be787628bb0275e3e776a56837
helciodasilva <helciodasilva@gmail.com>
2020-10-22 10:26:04 -0300

    Aula 4: Implementando comportamentos

--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -5,44 +5,95 @@ fun main() {
 
     var contaAlex = Conta()
     contaAlex.titular = "Alex"
-    contaAlex.saldo = 200.0
+    contaAlex.setSaldo(-200.0)
 
     val contaFran = Conta()
     contaFran.titular = "Fran"
     contaFran.numero = 1001
-    contaFran.saldo = 300.00
+    contaFran.setSaldo(300.00)
 
     println(contaFran.titular)
     println(contaFran.numero)
-    println(contaFran.saldo)
+    println(contaFran.getSaldo())
 
     println(contaAlex.titular)
     println(contaAlex.numero)
-    println(contaAlex.saldo)
+    println(contaAlex.getSaldo())
 
-    println("depositando na conta do Alex")
-    deposita(contaAlex, 50.0)
-    println(contaAlex.saldo)
+//    println("depositando na conta do Alex")
+//    contaFran.deposita(50.0)
+//    println(contaAlex.saldo)
+//
+//    println("depositando na conta da Fran")
+//    contaFran.deposita(70.0)
+//    println(contaFran.saldo)
+//
+//    println("Sacando na conta do Alex")
+//    contaAlex.saca(250.0)
+//    println(contaAlex.saldo)
+//
+//    println("Sacando na conta do Fran")
+//    contaFran.saca(100.0)
+//    println(contaFran.saldo)
+//
+//    println("saque em excesso na conta do Alex")
+//    contaAlex.saca(100.0)
+//    println(contaAlex.saldo)
+//
+//    println("saque em excesso na conta da Fran")
+//    contaFran.saca(500.0)
+//    println(contaFran.saldo)
+//
+//    println("Transferência da conta da Fran para o Alex")
+//
+//    if (contaFran.transfere(300.0, contaAlex)) {
+//        println("Transferência sucedida")
+//    } else {
+//        println("Falha na transferência")
+//    }
+//
+//    println(contaAlex.saldo)
+//    println(contaFran.saldo)
 
-    println("depositando na conta da Fran")
-    deposita(contaFran, 70.0)
-    println(contaFran.saldo)
 }
 
 class Conta {
     var titular = ""
     var numero = 0
-    var saldo = 0.0
+    private var saldo = 0.0
 
-    fun depositar() {
+    fun deposita(valor: Double) {
+        this.saldo += valor
+    }
+
+    fun saca(valor: Double) {
+        if (saldo >= valor) {
+            saldo -= valor
+        }
 
     }
 
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            saldo += valor
+            destino.deposita(valor)
+            return true;
+        } else {
+            return false
+        }
+    }
+
+    fun getSaldo(): Double {
+        return saldo
+    }
+
+    fun setSaldo(valor: Double){
+        if (valor > 0) {
+            saldo = valor
+        }
+    }
 
 }
-
-fun deposita(conta: Conta, valor: Double) {
-    conta.saldo += valor}
 
 fun testeCopiasEReferencias() {
     val numeroX = 10
