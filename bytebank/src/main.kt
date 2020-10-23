bd6115a5b782906073c272d33ffe909a14433e81
helciodasilva <helciodasilva@gmail.com>
2020-10-23 10:11:02 -0300

    04. Escrevendo código mais idiomático ao Kotlin

--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -3,70 +3,74 @@ fun main() {
     println("Bem vindo ao Bytebank")
     testaLacos()
 
-    var contaAlex = Conta()
-    contaAlex.titular = "Alex"
-    contaAlex.setSaldo(-200.0)
+    var contaAlex = Conta(titular = "Alex", numero = 1000)
+    contaAlex.deposita(-200.0)
 
-    val contaFran = Conta()
-    contaFran.titular = "Fran"
+    val contaFran = Conta(numero = 1001, titular = "Fran", )
     contaFran.numero = 1001
-    contaFran.setSaldo(300.00)
+    contaFran.deposita(300.00)
 
     println(contaFran.titular)
     println(contaFran.numero)
-    println(contaFran.getSaldo())
+    println(contaFran.saldo)
 
     println(contaAlex.titular)
     println(contaAlex.numero)
-    println(contaAlex.getSaldo())
+    println(contaAlex.saldo)
 
-//    println("depositando na conta do Alex")
-//    contaFran.deposita(50.0)
-//    println(contaAlex.saldo)
-//
-//    println("depositando na conta da Fran")
-//    contaFran.deposita(70.0)
-//    println(contaFran.saldo)
-//
-//    println("Sacando na conta do Alex")
-//    contaAlex.saca(250.0)
-//    println(contaAlex.saldo)
-//
-//    println("Sacando na conta do Fran")
-//    contaFran.saca(100.0)
-//    println(contaFran.saldo)
-//
-//    println("saque em excesso na conta do Alex")
-//    contaAlex.saca(100.0)
-//    println(contaAlex.saldo)
-//
-//    println("saque em excesso na conta da Fran")
-//    contaFran.saca(500.0)
-//    println(contaFran.saldo)
-//
-//    println("Transferência da conta da Fran para o Alex")
-//
-//    if (contaFran.transfere(300.0, contaAlex)) {
-//        println("Transferência sucedida")
-//    } else {
-//        println("Falha na transferência")
-//    }
-//
-//    println(contaAlex.saldo)
-//    println(contaFran.saldo)
+    println("depositando na conta do Alex")
+    contaFran.deposita(50.0)
+    println(contaAlex.saldo)
+
+    println("depositando na conta da Fran")
+    contaFran.deposita(70.0)
+    println(contaFran.saldo)
+
+    println("Sacando na conta do Alex")
+    contaAlex.saca(250.0)
+    println(contaAlex.saldo)
+
+    println("Sacando na conta do Fran")
+    contaFran.saca(100.0)
+    println(contaFran.saldo)
+
+    println("saque em excesso na conta do Alex")
+    contaAlex.saca(100.0)
+    println(contaAlex.saldo)
+
+    println("saque em excesso na conta da Fran")
+    contaFran.saca(500.0)
+    println(contaFran.saldo)
+
+    println("Transferência da conta da Fran para o Alex")
+
+    if (contaFran.transfere(destino = contaAlex, valor = 300.0)) {
+        println("Transferência sucedida")
+    } else {
+        println("Falha na transferência")
+    }
+
+    println(contaAlex.saldo)
+    println(contaFran.saldo)
 
 }
 
-class Conta {
-    var titular = ""
-    var numero = 0
-    private var saldo = 0.0
+class Conta(
+        var titular: String,
+        val numero: Int,
+        var teste: Int = 9
+) {
+    var saldo = 1.0
+        private set
 
     fun deposita(valor: Double) {
-        this.saldo += valor
+        if (valor > 0) {
+            this.saldo += valor
+        }
     }
 
     fun saca(valor: Double) {
+        ₢₢
         if (saldo >= valor) {
             saldo -= valor
         }
@@ -83,16 +87,6 @@ class Conta {
         }
     }
 
-    fun getSaldo(): Double {
-        return saldo
-    }
-
-    fun setSaldo(valor: Double){
-        if (valor > 0) {
-            saldo = valor
-        }
-    }
-
 }
 
 fun testeCopiasEReferencias() {
@@ -103,11 +97,8 @@ fun testeCopiasEReferencias() {
     println("numeroX $numeroX")
     println("numeroY $numeroY")
 
-    val contaJoao = Conta()
-    contaJoao.titular = "João"
-    var contaMaria = contaJoao
-    contaMaria.titular = "Maria"
-    contaMaria.titular = "João"
+    val contaJoao = Conta("João", 1002)
+    var contaMaria = Conta("Maria", 1003)
 
     println("titular conta joao: ${contaJoao.titular}")
     println("titular conta maria: ${contaJoao.titular}")
